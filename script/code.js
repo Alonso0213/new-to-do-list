fd600b7fd397efa9865e9cf25d5dbb49ee835f63
Alonso0213 <alonsocupido51@gmail.com>
2023-06-09 14:48:52 +0200

    not complete

--- script/code.js
+++ script/code.js
@@ -1,47 +1,65 @@
+
+let sorting = document.querySelector('#arrange');
 let addBtn = document.querySelector('#display');
-let list=[""]
-let output=document.querySelector('#show')
+let output=document.querySelector('#show');
+let removeBtn= document.querySelector('#del')
+let intxt= document.querySelector('#input').value
+let list=[]
+
 addBtn.addEventListener('click', (e)=>
 {
-    let intxt= document.querySelector('#input').value
+
+    
     output.innerHTML+=` <ul>
     <input type='checkbox'>
     ${intxt}
     <button>Edit</button>
-    <button id="remove" type="button">remove</button>
+    <button type="button" id="del">remove</button>
     </ul>
     `
-    display()
 })
-   
+
+function del() {
+    removeBtn.addEventListener('click',
+    )
     
-let sorting = document.querySelector('#arrange');
-sorting.addEventListener('click', (e) =>
-{
-    
+
 }
-)
 
-let removeBtn= document.querySelector('#remove')
 
-   removeBtn.addEventListener('click', (e)=>{
-    let output=document.querySelector('#show')
-    output.value = " "
-    
+   
 
-   }
-   )
-   function display() {
-    list.innerHTML =''
-    list.forEach((items)=>{
-        list.innerHTML+= `
-        <ul>
-        <input type='checkbox'>
-        ${items}
-        <button>Edit</button>
-        <button id="remove" type="button">remove</button>
-        </ul>
-        `
-    })
-    
-   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
